aaf6717a9905001e6b29b23770a68df742eb5ac6
rianeko12 <75882727+rianeko12@users.noreply.github.com>
2021-06-16 09:57:45 +0700

    Upload By Rian

---
+++ service/views/page/layanan.php
@@ -0,0 +1,50 @@
+<!-- SHOP LISTING-->
+<div class="col-lg-9 order-1 order-lg-2 mb-5 mb-lg-0">
+    <div class="col-md-4">
+        <input type="text" name="search_text" id="search_text" placeholder="Search" class="form-control" />
+
+    </div>
+
+
+    <div id="result"></div>
+    <div style="clear:both"></div>
+    <div class="row mb-3 align-items-center">
+
+    </div>
+
+    <div class="row" id="container">
+    <?php foreach ($mitra as $m) : ?>
+        <div class="col-md-4">
+            <form method="post" action="<?php echo base_url(); ?>shop/shopping/tambah" accept-charset="utf-8" id="result">
+                <div class="product text-center">
+                    <div class="mb-3 position-relative">
+                        <div class="badge text-white badge"></div>
+                        <a class="d-block">
+                            <img class="img-fluid" src="<?php echo base_url() . 'assets/gambar/mitra/'. $m->foto_mitra ?>" width="100%">
+                        </a>
+                        <div class="product-overlay">
+                            <ul class="mb-0 list-inline">
+                                <li class="list-inline-item m-0 p-0"><button type="submit" class="btn btn-sm btn-dark">Order</button></li>
+                                <li class="list-inline-item mr-0"><a class="btn btn-sm btn-outline-dark" href="<?php echo base_url(); ?>service/page/detail/"><i class="fas fa-expand"></i></a></li>
+
+                            </ul>
+                        </div>
+                    </div>
+                    <h6><a class="reset-anchor"><?php echo $m->daftar_keahlian?></a></h6>
+                    <p class="small text-muted"><?php echo $m->harga_jasa?></p>
+                </div>
+            </form>
+        </div>
+        <?php endforeach; ?>
+    </div>
+
+    <div style="margin-left: 330px;">
+        <?php echo $this->pagination->create_links(); ?>
+    </div>
+
+</div>
+</div>
+</div>
+</section>
+
+</div>
