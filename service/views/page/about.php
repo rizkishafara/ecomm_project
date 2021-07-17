583168df63b2b1b5db3e206eae553c1fec9dc32f
rianeko12 <75882727+rianeko12@users.noreply.github.com>
2021-07-17 17:15:38 +0700

    Upload By Rian

---
+++ service/views/page/about.php
@@ -0,0 +1,35 @@
+<div class="container">
+    <!-- HERO SECTION-->
+    <section class="py-5 bg-light">
+        <div class="container">
+            <div class="row px-4 px-lg-5 py-lg-4 align-items-center">
+                <div class="col-lg-6">
+                    <h1 class="h2 text-uppercase mb-0">About</h1>
+                </div>
+                <div class="col-lg-6 text-lg-right">
+                    <nav aria-label="breadcrumb">
+                        <ol class="breadcrumb justify-content-lg-end mb-0 px-0">
+                            <li class="breadcrumb-item"><a href="index.html">Home</a></li>
+                            <li class="breadcrumb-item active" aria-current="page">About</li>
+                        </ol>
+                    </nav>
+                </div>
+            </div>
+        </div>
+    </section>
+    <!-- SHOP LISTING-->
+    <div class="col-lg-9 order-1 order-lg-2 mb-5 mb-lg-0">
+        <div class="row mb-3 align-items-center">
+        </div>
+        <div class="row">
+            <h3>Layanan Jasa Online "Function Service"</h3>
+            <div class="text-justify">
+                <p>Berawal dari sebuah ide untuk menyelesaikan masalah pada kehidupan sehari-hari, terpikirkanlah untuk membuat sebuah aplikasi dimana Anda bisa menggunakan layanan jasa online tanpa keluar rumah.<br><br>Layanan Jasa Online hanya di <b>Function Service</b>, kami siap melayani!<br><br><i>" Mengapa harus memilih kami? "</i><br><br><b>1. Menyediakan Tenaga Kerja Profesional</b><br>Kami memahami kebutuhan Anda untuk mendapatkan pelayanan dengan orang yang berpengalaman serta profesional dalam bidangnya.<br><br><b>2. Mudah Dalam Penggunaan</b><br>Di desain secara user-friendly sehingga memudahkan pengunjung saat menggunakan aplikasi.<br><br><b>3. Tanpa Ribet & Nyaman</b><br>Berbagai fitur lengkap siap mendukung pengguna sehingga proses transaksi dapat dilakukan dengan cepat, tanpa ribet dan nyaman.<br><br></p>
+            </div>
+        </div>
+        <br>
+    </div>
+</div>
+</div>
+</section>
+</div>
